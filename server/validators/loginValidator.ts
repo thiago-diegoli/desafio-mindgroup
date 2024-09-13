fbbef6753a8ac466dab78734e480033e928f34db
Thiago Diegoli <thiago_diegoli@hotmail.com>
2024-09-13 17:52:49 -0300

    Implement user login/register

---
+++ server/validators/loginValidator.ts
@@ -0,0 +1,35 @@
+import { check } from 'express-validator';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const loginValidator = [
+  check('email')
+    .notEmpty().withMessage('O email é obrigatório')
+    .isEmail().withMessage('O email informado é inválido'),
+  check('password')
+    .notEmpty().withMessage('A senha é obrigatória')
+    .isLength({ min: 6 }).withMessage('A senha deve ter pelo menos 6 caracteres')
+];
+
+export const registerValidator = [
+  check('name')
+    .notEmpty().withMessage('O nome é obrigatório'),
+  check('email')
+    .notEmpty().withMessage('O email é obrigatório')
+    .isEmail().withMessage('O email informado é inválido')
+    .custom(async (email) => {
+      const user = await prisma.user.findUnique({
+        where: { email }
+      });
+      if (user) {
+        throw new Error('O email já está registrado');
+      }
+    }),
+  check('password')
+    .notEmpty().withMessage('A senha é obrigatória')
+    .isLength({ min: 6 }).withMessage('A senha deve ter pelo menos 6 caracteres'),
+  check('photo')
+    .optional()
+    .isBase64().withMessage('A foto deve estar no formato base64')
+];
