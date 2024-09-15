264d61034fb4ddbd3a8157a249b555131ad2f45e
Thiago Diegoli <thiago_diegoli@hotmail.com>
2024-09-15 17:50:15 -0300

    Fix routes protection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import ProjectDetails from './pages/ProjectDetails';
 
-function App() {
+const ProtectedRoute: React.FC<{ isAuthenticated: boolean }> = ({ isAuthenticated }) => {
+  if (!isAuthenticated) {
+    return <Navigate to="/signin" />;
+  }
+
+  return <Outlet />;
+};
+
+const App: React.FC = () => {
+  const isAuthenticated = Boolean(localStorage.getItem('access_token'));
+
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
         <Route path="/" element={<Navigate to="/signin" />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/project/:id" element={<ProjectDetails />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects/:id" element={<ProjectDetails />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
